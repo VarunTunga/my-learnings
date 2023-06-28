94b09571c8578a4977ba8d3a89810b36c2c94f0b
Varun Tunga <varuntunga.sangamone@gmail.com>
2023-06-28 13:08:05 +0530

    initial commit

---
+++ 6.py
@@ -0,0 +1,12 @@
+#filename="in1.txt"
+#f1=open(filename,"r")
+num1=int(input(" enter the number1 "))
+num2=int(input(" enter the number2 "))
+sum1=num1+num2
+diff1=num1-num2
+prod1=num1*num2
+div1=num1/num2
+div2=num1//num2
+mod1=num1%num2
+exp1=num1**num2
+print(sum1,diff1,prod1,div1,div2,mod1,exp1)
